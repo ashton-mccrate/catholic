465ad72e29444339be9fcff79b5a509b4faabe4b
Ashton McCrate <a.mccrate@gmail.com>
2015-11-27 12:25:45 -0800

    sin is original and can be absolved with repentance

--- lib/catholic.rb
+++ lib/catholic.rb
@@ -3,8 +3,19 @@ require "catholic/version"
 module Catholic
   module ObjectExtensions
 
+    attr_accessor :sin
+
+    def initialize
+      self.sin = true
+      super()
+    end
+
     def has_sin?
-      return true
+      return self.sin ? true : false
+    end
+
+    def repent
+      self.sin = false
     end
 
   end
--- test/catholic_test.rb
+++ test/catholic_test.rb
@@ -3,8 +3,19 @@ require 'catholic'
 
 class CatholicTest < MiniTest::Test
 
+  def test_new_objects_have_original_sin
+    object = Object.new
+    assert object.sin, 'Your object was born without sin'
+  end
+
   def test_has_sin?
-    assert Object.new.has_sin?
+    assert Object.new.has_sin?, 'Object has no sin'
+  end
+
+  def test_repenting_absolves_sin
+    o = Object.new
+    o.repent
+    assert o.has_sin? == false, 'Repenting did not absolve object of it\'s sin'
   end
 
 end
